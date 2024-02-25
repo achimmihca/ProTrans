6088c62ba6f92453d8de50a5c777032b6f630ffc
Andreas Stange <anst.dev@gmail.com>
2024-02-25 13:26:02 +0100

    moved example for component based UI text translation to Assets, out of the package

--- ProTrans/Assets/Samples/ComponentBasedUnityUi/TranslatedText.cs
+++ ProTrans/Assets/Samples/ComponentBasedUnityUi/TranslatedText.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace ProTrans
 {
     [RequireComponent(typeof(Text))]
     [ExecuteInEditMode]
-    public class TranslatedText : AbstractTranslatorBehaviour
+    public class TranslatedText : MonoBehaviour
     {
         [Delayed]
         public string key;
-        private string lastKey;
 
         private Text uiText;
-        
+        private string lastKey;
+
+        private void Start()
+        {
+            UpdateTranslation();
+        }
+
 #if UNITY_EDITOR
         private void Update()
         {
@@ -25,34 +29,22 @@ namespace ProTrans
         }
 #endif
 
-        public override void UpdateTranslation()
+        public void UpdateTranslation()
         {
-            if (key == null)
-            {
-                Debug.LogWarning($"Missing translation key for object '{gameObject.name}'", gameObject);
-                return;
-            }
-
-            string trimmedKey = key.Trim();
+            string trimmedKey = key?.Trim();
             if (string.IsNullOrEmpty(trimmedKey))
             {
                 Debug.LogWarning($"Missing translation key for object '{gameObject.name}'", gameObject);
                 return;
             }
 
-            Dictionary<string, string> translationArguments = GetTranslationArguments();
-            string translation = TranslationManager.GetTranslation(trimmedKey, translationArguments);
-            
+            string translation = TranslationManager.GetTranslation(trimmedKey);
+
             if (uiText == null)
             {
                 uiText = GetComponent<Text>();
             }
             uiText.text = translation;
-        }
-
-        protected virtual Dictionary<string, string> GetTranslationArguments()
-        {
-            return null;
         }
     }
 }
